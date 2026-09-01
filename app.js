057465561c07899e6b6aa8b539482ed88c82739a
SYNC <aibarragonzalez2008@gmail.com>
2026-09-01 11:26:17 -0400

    Actualizacion sitio

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-import express from 'express'
+const express = require('express');
 
 const app = express();
 
+const PORT = 3000;
+
+
+// Configurar EJS
+app.set('view engine', 'ejs');
+
+// RUTAS
 app.get('/', (req, res) => {
-  res.send('Compa Toroooooooooo');
+    res.render('index', { title: 'Página de Inicio' });
 });
 
-app.get('/'), (req, res) => {
-    res.send('servicios');
-};
+app.get('/servicios', (req, res) => {
+    res.render('servicios');
+});
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+app.get('/contacto', (req, res) => {
+    res.render('contacto');
+});
+
+app.get('/nosotros', (req, res) => {
+    res.render('nosotros');
+});
+
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
